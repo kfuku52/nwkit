9f254272748e04d65f6fdfe4649de5e10fb7abac
Kenji Fukushima <kfuku52@gmail.com>
2022-02-13 16:55:17 +0100

    supported nwkit nhx2nwk --node_label

--- nwkit/nhx2nwk.py
+++ nwkit/nhx2nwk.py
@@ -2,4 +2,10 @@ from nwkit.util import *
 
 def nhx2nwk_main(args):
     tree = read_tree(args.infile, args.format)
+    if (args.node_label!=''):
+        for node in tree.traverse():
+            if node.is_leaf():
+                continue
+            if args.node_label in dir(node):
+                node.name = getattr(node, args.node_label)
     write_tree(tree, args, format=args.format)
