3fda29e000843e1ba56735bf01617cebb645ca5c
Kenji Fukushima <kfuku52@gmail.com>
2024-02-18 23:14:16 +0100

    bug fix

--- nwkit/prune.py
+++ nwkit/prune.py
@@ -3,8 +3,9 @@ from nwkit.util import *
 
 def prune_main(args):
     tree = read_tree(args.infile, args.format, args.quoted_node_names)
+    for node in tree.traverse():
+        node.flag_prune = False
     for leaf in tree.iter_leaves():
-        leaf.flag_prune = False
         if args.invert_match:
             if not re.fullmatch(args.pattern, leaf.name):
                 leaf.flag_prune = True
@@ -15,9 +16,12 @@ def prune_main(args):
         if node.is_leaf():
             if node.flag_prune:
                 sys.stderr.write(f'Pruning {node.name}\n')
-                node.delete()
+                node.flag_prune = True
         else:
             if all([ leaf.flag_prune for leaf in node.iter_leaves() ]):
-                node.delete()
+                node.flag_prune = True
+    for node in tree.traverse(strategy='preorder'):
+        if node.flag_prune:
+            node.delete(preserve_branch_length=True)
     tree = remove_singleton(tree, verbose=False, preserve_branch_length=True)
     write_tree(tree, args, format=args.outformat)
