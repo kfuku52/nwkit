6c362f2ab064fd482e32d0964b17412ceafabafd
mkrg01 <hamudtmy755@icloud.com>
2025-10-16 12:08:14 +0900

    Support --only_contrastive_clades

--- nwkit/skim.py
+++ nwkit/skim.py
@@ -24,7 +24,7 @@ def mark_traits_to_nodes(tree, trait_df, args):
         if node.is_leaf():
             node.add_feature('trait', leafname2trait[node.name])
         else:
-            trait = pandas.NA
+            trait = None
             for child in node.children:
                 if pandas.isna(child.trait):
                     continue
@@ -50,10 +50,9 @@ def iter_frontier_nodes(root, stop_condition):
             continue
         stack.extend(reversed(node.children))
 
-def add_group_ids(trait_df, tree, args):
+def add_group_ids(trait_df, marked_tree):
     group_id = 1
     leafname2group = {}
-    marked_tree = mark_traits_to_nodes(tree, trait_df, args)
     for node in iter_frontier_nodes(root=marked_tree.get_tree_root(), stop_condition=lambda node: pandas.isna(node.trait) or node.trait != '_MIXED_'):
         if node.is_leaf():
             leafname2group[node.name] = group_id
@@ -88,14 +87,44 @@ def sample_from_groups(trait_df, args):
                 raise ValueError(f"Invalid value for '--filter_mode': {args.filter_mode}")
     return sampled_df.merge(trait_df[['leaf_name', 'group']], on=['leaf_name'])
 
+def iter_contrastive_nodes(root):
+    if root.trait != '_MIXED_':
+        return
+    stack = [root]
+    while stack:
+        node = stack.pop()
+        flag_has_mixed_child = False
+        for child in node.children:
+            if child.trait == '_MIXED_':
+                stack.append(child)
+                flag_has_mixed_child = True
+        if not flag_has_mixed_child:
+            yield node
+
+def add_contrastive_clade_ids(trait_df, marked_tree):
+    contrastive_clade_id = 1
+    leafname2contrastive_clade = {}
+    for node in iter_contrastive_nodes(root=marked_tree.get_tree_root()):
+        for leaf_name in node.get_leaf_names():
+            leafname2contrastive_clade[leaf_name] = contrastive_clade_id
+        contrastive_clade_id += 1
+    trait_df['contrastive_clade'] = trait_df['leaf_name'].map(leafname2contrastive_clade).astype('Int64')
+    return trait_df
+
 def skim_main(args):
     tree = read_tree(args.infile, args.format, args.quoted_node_names)
     trait_df = read_trait(args, tree)
-    trait_df = add_group_ids(trait_df, tree, args)
-    sampled_trait_df = sample_from_groups(trait_df, args)
-    tree.prune(sampled_trait_df['leaf_name'].tolist(), preserve_branch_length=True)
+    marked_tree = mark_traits_to_nodes(tree, trait_df, args)
+    trait_df = add_group_ids(trait_df, marked_tree)
+    if args.only_contrastive_clades:
+        trait_df = add_contrastive_clade_ids(trait_df, marked_tree)
+        sampled_trait_df = sample_from_groups(trait_df, args)
+        sampled_trait_df = sampled_trait_df[~sampled_trait_df['contrastive_clade'].isna()]
+    else:
+        sampled_trait_df = sample_from_groups(trait_df, args)
     if args.output_groupfile:
         filename = args.outfile.removesuffix('.nwk')
         trait_df.sort_values('group').to_csv(f'{filename}.all.tsv', sep='\t', index=False)
         sampled_trait_df.sort_values('group').to_csv(f'{filename}.sampled.tsv', sep='\t', index=False)
+    tree.prune(sampled_trait_df['leaf_name'].tolist(), preserve_branch_length=True)
     write_tree(tree, args, format=args.outformat)
